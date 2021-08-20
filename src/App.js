332293d3f8a991c86b280132fa26f5a53bf29312
Londolani <Londolani0700@gmail.com>
2021-08-20 20:30:06 +0200

    added image folder

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import './App.css'
 
 class App extends React.Component{
   render(){
-    return(
-      <h1>Londolani Ndou</h1>
+    return( 
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <div className="card">
+              <img src="https://i.pravatar.cc/150?img=52" class="card-img-top" alt="..."/>
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
---
+++ src/Components/card.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const card = () => {
+    return(
+        <h1>hi</h1>
+    );
+}
+
+export default card;
