de3d1398066b3411d66359add08b6ece71690c3f
Londolani <Londolani0700@gmail.com>
2021-08-20 21:29:54 +0200

    added second state and finished

--- src/App.js
+++ src/App.js
@@ -5,26 +5,30 @@ import Card from './Components/card';
 class App extends React.Component{
 
   state = {
-    avatar: 51
+    avatar: 51,
+    personName: 'James Smith' 
   }
 
   changeAvatar = () =>{
     this.setState({
-      avatar:65
+      avatar:65,
+      personName: 'Oliver Turn'
     })
   }
+
+
 
   render(){
     return( 
       <div className="container">
         <div className="row">
-          <Card name="James Smith" job="Pilot" avatar={this.state.avatar} />
+          <Card name={this.state.personName} job="Pilot" avatar={this.state.avatar} />
           <Card name="Jessica Jones" job="Nurse" avatar={28}/>
           <Card name="Tom Hanks"job="Lawyer" avatar={68}/>
         </div>
         <div className="row">
           <div className="col-md-4">
-            <a onClick={this.changeAvatar} href="#" className="changeAvatar">Change Avatar</a>
+            <a onClick={this.changeAvatar} href="#" className="changeAvatar">Change Details</a>
           </div>
         </div>
 
