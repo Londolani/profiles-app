a3cc903e32da8f5213c50491c422e37bb5875eb5
Londolani <Londolani0700@gmail.com>
2021-08-20 21:07:32 +0200

    created components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
-import React from 'react'
-import './App.css'
+import React from 'react';
+import './App.css';
+import Card from './Components/card';
 
 class App extends React.Component{
   render(){
     return( 
       <div className="container">
         <div className="row">
-          <div className="col-md-4">
-            <div className="card">
-              <img src="https://i.pravatar.cc/150?img=52" class="card-img-top" alt="..."/>
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
-          </div>
+          <Card name="James Smith" job="Pilot" avatar={51} />
+          <Card name="Jessica Jones" job="Nurse" avatar={28}/>
+          <Card name="Tom Hanks"job="Lawyer" avatar={61}/>
         </div>
       </div>
     );
--- src/Components/card.js
+++ src/Components/card.js
@@ -1,8 +1,19 @@
 import React from 'react';
 
-const card = () => {
+const card = (props) => {
+    const avatar = `https://i.pravatar.cc/150?img=${props.name}`;
+    //const avatar = "https://i.pravatar.cc/150?img="+ props.avatar+"";
     return(
-        <h1>hi</h1>
+        <div className="col-md-4">
+            <div className="card">
+              <img src={avatar} className="card-img-top" alt="..."/>
+              <div className="card-body">
+                <h5 className="card-title">{props.name}</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">{props.job}</a>
+              </div>
+            </div>
+          </div>
     );
 }
 
